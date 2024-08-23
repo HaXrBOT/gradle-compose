f654b4d4069e141f6c8098c376d0ebd4b998aa50
HaXr <haxr@outlook.dk>
2024-08-23 16:40:34 +0200

    Yoinked stuff from upstream and reintegrated naming and dependency changes
    This includes support for 1.21.1.

--- src/main/java/dev/tr7zw/gradle_compose/App.java
+++ src/main/java/dev/tr7zw/gradle_compose/App.java
@@ -1,44 +1,48 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 package dev.tr7zw.gradle_compose;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStreamReader;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
-
-import org.fusesource.jansi.AnsiConsole;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonIOException;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonSyntaxException;
-
-import dev.tr7zw.gradle_compose.ComposeData.Project;
 import dev.tr7zw.gradle_compose.provider.SourceProvider;
 import dev.tr7zw.gradle_compose.util.ConfigUtil;
 import dev.tr7zw.gradle_compose.util.FileProcessingUtil;
 import dev.tr7zw.gradle_compose.util.FileUtil;
 import dev.tr7zw.gradle_compose.util.GitUtil;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import org.fusesource.jansi.AnsiConsole;
 
 public class App {
 	public final String version = "0.0.5";
 
+	public App() {
+	}
+
 	public static void main(String[] args) {
-		new App().run(args);
+		(new App()).run(args);
 	}
 
 	public void run(String[] args) {
-		setup();
-		printWelcome();
-		applyExecFlag();
+		this.setup();
+		this.printWelcome();
+		this.applyExecFlag();
 		ComposeData data = ConfigUtil.loadLocalConfig();
 		SourceProvider provider = FileUtil.getProvider(data);
 		TemplateData template = ConfigUtil.getTemplateData(provider, data);
-		addAutoReplacements(data, template);
-		processComposition(data, template, provider);
+		this.addAutoReplacements(data, template);
+		this.processComposition(data, template, provider);
 		provider.markAsDone();
 	}
 
@@ -47,91 +51,119 @@ public class App {
 	}
 
 	private void printWelcome() {
-		System.out.println("Loading gradle-compose V" + version + "...");
+		System.out.println("Loading gradle-compose V0.0.5...");
 	}
 
 	private void applyExecFlag() {
 		if (GitUtil.gitAvailable()) {
 			System.out.println("Update exec flag of gradlecw...");
-			GitUtil.runGitCommand(new File("."), new String[] { "git", "add", "--chmod=+x", "./gradlecw" });
+			GitUtil.runGitCommand(new File("."), new String[]{"git", "add", "--chmod=+x", "./gradlecw"});
 		}
+
 	}
 
 	private void addAutoReplacements(ComposeData data, TemplateData template) {
 		StringBuilder includes = new StringBuilder();
-		for (String entry : data.subProjects.keySet()) {
+		Iterator var4 = data.subProjects.keySet().iterator();
+
+		while(var4.hasNext()) {
+			String entry = (String)var4.next();
 			includes.append("include(\"" + entry + "\")\n");
 		}
+
 		data.replacements.put("autoincludes", includes.toString());
 		StringBuilder githubWorkFlow = new StringBuilder();
-		for (String entry : data.subProjects.keySet()) {
+		Iterator var14 = data.subProjects.keySet().iterator();
+
+		while(var14.hasNext()) {
+			String entry = (String)var14.next();
 			githubWorkFlow.append("            " + entry + "/build/libs/*\n");
 		}
+
 		data.replacements.put("autoworkflowfiles", githubWorkFlow.toString());
 		StringBuilder forkGithubWorkFlow = new StringBuilder();
-		for (String entry : data.subProjects.keySet()) {
+		Iterator var16 = data.subProjects.keySet().iterator();
+
+		while(var16.hasNext()) {
+			String entry = (String)var16.next();
 			forkGithubWorkFlow.append("            workspace/" + entry + "/build/libs/*\n");
 		}
+
 		data.replacements.put("githubautoworkflowfiles", forkGithubWorkFlow.toString());
-
-		if (data.enabledFlags.contains("autopublish") && new File("settings.json").exists()) {
-			Map<String, String> baseReplacements = FileProcessingUtil.mergeReplacements(data.replacements,
-					template.defaultReplacements);
-
+		if (data.enabledFlags.contains("autopublish") && (new File("settings.json")).exists()) {
+			Map<String, String> baseReplacements = FileProcessingUtil.mergeReplacements(new Map[]{data.replacements, template.defaultReplacements});
 			StringBuilder releaseText = new StringBuilder();
+
 			try {
-				JsonObject settingsData = new Gson().fromJson(
-						new InputStreamReader(new FileInputStream(new File("settings.json"))), JsonObject.class);
-				for (JsonElement el : settingsData.get("versions").getAsJsonArray()) {
-					String version = el.getAsString();
-					if (data.enabledFlags.contains("curseforge")) {
-						// Curseforge releases
-						releaseText.append("      - name: Publish-" + version + "-Curseforge\n");
-						releaseText.append("        uses: Kir-Antipov/mc-publish@v"
-								+ baseReplacements.get("mcpublishVersion") + "\n");
-						releaseText.append("        with:\n");
-						releaseText.append("          curseforge-id: " + data.replacements.get("curseforgeid") + "\n");
-						releaseText.append("          curseforge-token: ${{ secrets.CURSEFORGE_TOKEN }}\n");
-						releaseText.append("          loaders: " + getModloaderName(version).toLowerCase() + "\n");
-						releaseText.append("          name: " + data.replacements.get("name") + " v${{github.ref_name}} for "+ getMCVersion(version) + "\n");
-						if (isForgelike(version)) {
-							releaseText.append("          version-type: beta\n");
+				JsonObject settingsData = (JsonObject)(new Gson()).fromJson(new InputStreamReader(new FileInputStream(new File("settings.json"))), JsonObject.class);
+				Iterator var9 = settingsData.get("versions").getAsJsonArray().iterator();
+
+				while(true) {
+					String version;
+					do {
+						if (!var9.hasNext()) {
+							data.replacements.put("autoreleasesteps", releaseText.toString());
+							return;
 						}
-						releaseText.append("          files: 'versions/" + version
-								+ "/build/libs/!(*-@(dev|sources|javadoc|all)).jar'\n");
-						releaseText.append("          game-versions: " + getMCVersion(version) + "\n");
-						releaseText.append("          dependencies: " + data.replacements.get("mcpublish_dependencies") + "\n");
+
+						JsonElement el = (JsonElement)var9.next();
+						version = el.getAsString();
+						if (data.enabledFlags.contains("curseforge")) {
+							releaseText.append("      - name: Publish-" + version + "-Curseforge\n");
+							releaseText.append("        uses: Kir-Antipov/mc-publish@v" + (String)baseReplacements.get("mcpublishVersion") + "\n");
+							releaseText.append("        with:\n");
+							releaseText.append("          curseforge-id: " + (String)data.replacements.get("curseforgeid") + "\n");
+							releaseText.append("          curseforge-token: ${{ secrets.CURSEFORGE_TOKEN }}\n");
+							releaseText.append("          loaders: " + this.getModloaderName(version).toLowerCase() + "\n");
+							releaseText.append("          name: " + data.replacements.get("name") + " v${{github.ref_name}} for "+ this.getModloaderName(version) + " " + this.getMCVersion(version) + "\n");
+							if (this.isForgelike(version)) {
+								releaseText.append("          version-type: beta\n");
+							}
+
+							if (!"1.20.6-forge".equals(version) && !"1.21-forge".equals(version)) {
+								releaseText.append("          files: 'versions/" + version + "/build/libs/!(*-@(dev|sources|javadoc|all)).jar'\n");
+							} else {
+								releaseText.append("          files: 'versions/" + version + "/build/libs/!(*-@(dev|sources|javadoc)).jar'\n");
+							}
+
+							releaseText.append("          game-versions: " + this.getMCVersionDependency(version) + "\n");
+							releaseText.append("          dependencies: " + data.replacements.get("mcpublish_dependencies") + "\n");
+						}
+					} while(!data.enabledFlags.contains("modrinth"));
+
+					releaseText.append("      - name: Publish-" + version + "-Modrinth\n");
+					releaseText.append("        uses: Kir-Antipov/mc-publish@v" + (String)baseReplacements.get("mcpublishVersion") + "\n");
+					releaseText.append("        with:\n");
+					releaseText.append("          modrinth-id: " + (String)data.replacements.get("modrinthid") + "\n");
+					releaseText.append("          modrinth-token: ${{ secrets.MODRINTH_TOKEN }}\n");
+					releaseText.append("          loaders: " + this.getModloaderName(version).toLowerCase() + "\n");
+					releaseText.append("          name: " + data.replacements.get("name") + " v${{github.ref_name}} for "+ this.getModloaderName(version) + " " + this.getMCVersion(version) + "\n");
+					if (!"1.20.6-forge".equals(version) && !"1.21-forge".equals(version)) {
+						releaseText.append("          files: 'versions/" + version + "/build/libs/!(*-@(dev|sources|javadoc|all)).jar'\n");
+					} else {
+						releaseText.append("          files: 'versions/" + version + "/build/libs/!(*-@(dev|sources|javadoc)).jar'\n");
 					}
-					if (data.enabledFlags.contains("modrinth")) {
-						// Modrinth releases
-						releaseText.append("      - name: Publish-" + version + "-Modrinth\n");
-						releaseText.append("        uses: Kir-Antipov/mc-publish@v"
-								+ baseReplacements.get("mcpublishVersion") + "\n");
-						releaseText.append("        with:\n");
-						releaseText.append("          modrinth-id: " + data.replacements.get("modrinthid") + "\n");
-						releaseText.append("          modrinth-token: ${{ secrets.MODRINTH_TOKEN }}\n");
-						releaseText.append("          loaders: " + getModloaderName(version).toLowerCase() + "\n");
-						releaseText.append("          name: " + data.replacements.get("name") + " v${{github.ref_name}} for "+ getMCVersion(version) + "\n");
-						releaseText.append("          files: 'versions/" + version
-								+ "/build/libs/!(*-@(dev|sources|javadoc|all)).jar'\n");
-						releaseText.append("          game-versions: " + getMCVersion(version) + "\n");
-						releaseText.append("          dependencies: " + data.replacements.get("mcpublish_dependencies") + "\n");
-					}
+
+					releaseText.append("          game-versions: " + this.getMCVersionDependency(version) + "\n");
+					releaseText.append("          dependencies: " + data.replacements.get("mcpublish_dependencies") + "\n");
 				}
-				data.replacements.put("autoreleasesteps", releaseText.toString());
-			} catch (JsonSyntaxException | JsonIOException | FileNotFoundException e) {
-				e.printStackTrace();
+			} catch (JsonIOException | FileNotFoundException | JsonSyntaxException var12) {
+				var12.printStackTrace();
 			}
 		}
+
+	}
+
+	private String getMCVersionDependency(String version) {
+		return "1.21".equals(version.split("-")[0]) ? "1.21.x" : version.split("-")[0];
 	}
 
 	private String getMCVersion(String version) {
-		return version.split("-")[0];
+		return "1.21".equals(version.split("-")[0]) ? "1.21(.1)" : version.split("-")[0];
 	}
 
 	private String getModloaderName(String version) {
-		return version.toLowerCase().contains("fabric") ? "Fabric"
-				: version.toLowerCase().contains("neoforge") ? "NeoForge" : "Forge";
+		return version.toLowerCase().contains("fabric") ? "Fabric" : (version.toLowerCase().contains("neoforge") ? "NeoForge" : "Forge");
 	}
 
 	private boolean isForgelike(String version) {
@@ -139,70 +171,56 @@ public class App {
 	}
 
 	private void processComposition(ComposeData data, TemplateData template, SourceProvider provider) {
-		// copy in ./gradle files
 		FileUtil.copyIfAvailable(provider, "gradle/wrapper/gradle-wrapper.properties");
 		FileUtil.copyIfAvailable(provider, "gradle/wrapper/gradle-wrapper.jar");
-		Map<String, String> baseReplacements = FileProcessingUtil.mergeReplacements(data.replacements,
-				template.defaultReplacements);
-		updateProject(new File("."), baseReplacements, provider, data.rootProject, template.availableFlags,
-				data.enabledFlags);
-		for (Entry<String, Project> entry : data.subProjects.entrySet()) {
-			updateProject(new File(".", entry.getKey()), baseReplacements, provider, entry.getValue(),
-					template.availableFlags, data.enabledFlags);
+		Map<String, String> baseReplacements = FileProcessingUtil.mergeReplacements(new Map[]{data.replacements, template.defaultReplacements});
+		this.updateProject(new File("."), baseReplacements, provider, data.rootProject, template.availableFlags, data.enabledFlags);
+		Iterator var5 = data.subProjects.entrySet().iterator();
+
+		while(var5.hasNext()) {
+			Map.Entry<String, ComposeData.Project> entry = (Map.Entry)var5.next();
+			this.updateProject(new File(".", (String)entry.getKey()), baseReplacements, provider, (ComposeData.Project)entry.getValue(), template.availableFlags, data.enabledFlags);
 		}
+
 		if (data.version.equals("0.0.1")) {
 			Set<String> customEntries = ConfigUtil.readCustomList(provider, "custom.compose");
-			for (String name : customEntries) {
-				FileUtil.copyIfAvailableWithReplacments(provider, new File("."), data.rootProject.template, name,
-						FileProcessingUtil.mergeReplacements(data.rootProject.replacements, baseReplacements),
-						template.availableFlags, data.enabledFlags);
+			Iterator var9 = customEntries.iterator();
+
+			while(var9.hasNext()) {
+				String name = (String)var9.next();
+				FileUtil.copyIfAvailableWithReplacments(provider, new File("."), data.rootProject.template, name, FileProcessingUtil.mergeReplacements(new Map[]{data.rootProject.replacements, baseReplacements}), template.availableFlags, data.enabledFlags);
 			}
 		} else {
-			for (String name : template.customEntries) {
-				FileUtil.copyIfAvailableWithReplacments(provider, new File("."), data.rootProject.template, name,
-						FileProcessingUtil.mergeReplacements(data.rootProject.replacements, baseReplacements),
-						template.availableFlags, data.enabledFlags);
+			var5 = template.customEntries.iterator();
+
+			while(var5.hasNext()) {
+				String name = (String)var5.next();
+				FileUtil.copyIfAvailableWithReplacments(provider, new File("."), data.rootProject.template, name, FileProcessingUtil.mergeReplacements(new Map[]{data.rootProject.replacements, baseReplacements}), template.availableFlags, data.enabledFlags);
 			}
 		}
-		processFork(baseReplacements, provider, data.rootProject, template.availableFlags, data.enabledFlags);
+
+		this.processFork(baseReplacements, provider, data.rootProject, template.availableFlags, data.enabledFlags);
 	}
 
-	private void updateProject(File baseDir, Map<String, String> replacements, SourceProvider provider, Project project,
-			Set<String> availableTags, Set<String> enabledTags) {
-		Map<String, String> projectReplacements = FileProcessingUtil.mergeReplacements(project.replacements,
-				replacements);
-		FileUtil.copyIfAvailableWithReplacments(provider, baseDir, project.template, "github/workflows/build.yml",
-				".github/workflows/build.yml", projectReplacements, availableTags, enabledTags);
-		FileUtil.copyIfAvailableWithReplacments(provider, baseDir, project.template, "github/workflows/tag.yml",
-				".github/workflows/tag.yml", projectReplacements, availableTags, enabledTags);
-		FileUtil.copyIfAvailableWithReplacments(provider, baseDir, project.template, "github/FUNDING.yml",
-				".github/FUNDING.yml", projectReplacements, availableTags, enabledTags);
-		FileUtil.copyIfAvailableWithReplacments(provider, baseDir, project.template, "build.gradle",
-				projectReplacements, availableTags, enabledTags);
-		FileUtil.copyIfAvailableWithReplacments(provider, baseDir, project.template, "gradle.properties",
-				projectReplacements, availableTags, enabledTags);
-		FileUtil.copyIfAvailableWithReplacments(provider, baseDir, project.template, "settings.gradle",
-				projectReplacements, availableTags, enabledTags);
+	private void updateProject(File baseDir, Map<String, String> replacements, SourceProvider provider, ComposeData.Project project, Set<String> availableTags, Set<String> enabledTags) {
+		Map<String, String> projectReplacements = FileProcessingUtil.mergeReplacements(new Map[]{project.replacements, replacements});
+		FileUtil.copyIfAvailableWithReplacments(provider, baseDir, project.template, "github/workflows/build.yml", ".github/workflows/build.yml", projectReplacements, availableTags, enabledTags);
+		FileUtil.copyIfAvailableWithReplacments(provider, baseDir, project.template, "github/workflows/tag.yml", ".github/workflows/tag.yml", projectReplacements, availableTags, enabledTags);
+		FileUtil.copyIfAvailableWithReplacments(provider, baseDir, project.template, "github/FUNDING.yml", ".github/FUNDING.yml", projectReplacements, availableTags, enabledTags);
+		FileUtil.copyIfAvailableWithReplacments(provider, baseDir, project.template, "build.gradle", projectReplacements, availableTags, enabledTags);
+		FileUtil.copyIfAvailableWithReplacments(provider, baseDir, project.template, "gradle.properties", projectReplacements, availableTags, enabledTags);
+		FileUtil.copyIfAvailableWithReplacments(provider, baseDir, project.template, "settings.gradle", projectReplacements, availableTags, enabledTags);
 	}
 
-	private void processFork(Map<String, String> replacements, SourceProvider provider, Project project,
-			Set<String> availableTags, Set<String> enabledTags) {
+	private void processFork(Map<String, String> replacements, SourceProvider provider, ComposeData.Project project, Set<String> availableTags, Set<String> enabledTags) {
 		File parentDir = new File("..");
-		if (new File(parentDir, "patches").exists() && new File(parentDir, "upstream").exists()
-				&& new File(parentDir, "workspace").exists() && new File(parentDir, "repo").exists()
-				&& new File(parentDir, "sha").exists()) {
+		if ((new File(parentDir, "patches")).exists() && (new File(parentDir, "upstream")).exists() && (new File(parentDir, "workspace")).exists() && (new File(parentDir, "repo")).exists() && (new File(parentDir, "sha")).exists()) {
 			System.out.println("Updating fork .github folder...");
-			Map<String, String> projectReplacements = FileProcessingUtil.mergeReplacements(project.replacements,
-					replacements);
-			FileUtil.copyIfAvailableWithReplacments(provider, parentDir, project.template,
-					"github/forkworkflows/build.yml", ".github/workflows/build.yml", projectReplacements, availableTags,
-					enabledTags);
-			FileUtil.copyIfAvailableWithReplacments(provider, parentDir, project.template,
-					"github/forkworkflows/tag.yml", ".github/workflows/tag.yml", projectReplacements, availableTags,
-					enabledTags);
-			FileUtil.copyIfAvailableWithReplacments(provider, parentDir, project.template, "github/FUNDING.yml",
-					".github/FUNDING.yml", projectReplacements, availableTags, enabledTags);
+			Map<String, String> projectReplacements = FileProcessingUtil.mergeReplacements(new Map[]{project.replacements, replacements});
+			FileUtil.copyIfAvailableWithReplacments(provider, parentDir, project.template, "github/forkworkflows/build.yml", ".github/workflows/build.yml", projectReplacements, availableTags, enabledTags);
+			FileUtil.copyIfAvailableWithReplacments(provider, parentDir, project.template, "github/forkworkflows/tag.yml", ".github/workflows/tag.yml", projectReplacements, availableTags, enabledTags);
+			FileUtil.copyIfAvailableWithReplacments(provider, parentDir, project.template, "github/FUNDING.yml", ".github/FUNDING.yml", projectReplacements, availableTags, enabledTags);
 		}
-	}
 
+	}
 }
